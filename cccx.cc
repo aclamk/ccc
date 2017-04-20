f5846ca01b4e615f169e8018edd21a5902f5e531
Adam Kupczyk <akupczyk@mirantis.com>
2017-04-20 13:07:11 +0200

    changes

--- cccx.cc
+++ cccx.cc
@@ -54,15 +54,16 @@ char peek(char* p){return (*p);};
 
 
 uint64_t multi_thread_regular(
-    std::vector<int>& cpus)
+			      std::vector<int>& cpus,
+			      bool share=true)
 {
   std::atomic<uint32_t> first_done[cpus.size()];
   for(auto& a:first_done)
     a.store(0);
   std::atomic<uint64_t> time_a(0);
 
-  alignas(cache_line_size) char buffer[cache_line_size]={0,};
-  memset(buffer,0, cache_line_size);
+  alignas(cache_line_size) char buffer[cache_line_size*sizeof(cpus)]={0,};
+  memset(buffer, 0, cache_line_size);
 
   std::vector<std::thread> threads;
   int cpu_count = cpus.size();
@@ -72,6 +73,9 @@ uint64_t multi_thread_regular(
       threads.push_back(
 	std::thread([&,c]()
       {
+	char* buffer_in=buffer;
+	if (!share)
+	  buffer_in = &buffer[cache_line_size*c];
 	set_affinity(cpus[c]);
 	uint64_t time = 0;
 	for (size_t k=0; k<hard_repeats; k++)
@@ -84,7 +88,7 @@ uint64_t multi_thread_regular(
 	    size_t j=c;
 	    for (size_t i=0; i<64*256*100; i+=cpu_count)
 	      {
-		inc(&buffer[j]);
+		inc(&buffer_in[j]);
 		j+=cpu_count;
 		if (j>=range) j-=range;
 	      }
@@ -104,7 +108,8 @@ uint64_t multi_thread_regular(
 
 
 uint64_t multi_thread_atomic(
-    std::vector<int>& cpus)
+			     std::vector<int>& cpus,
+			     bool share=true)
 {
   std::atomic<uint32_t> first_done[cpus.size()];
   for(auto& a:first_done)
@@ -112,7 +117,7 @@ uint64_t multi_thread_atomic(
 
   std::atomic<uint64_t> time_a(0);
 
-  alignas(cache_line_size) std::atomic<int8_t> atomiki[cache_line_size];
+  alignas(cache_line_size) std::atomic<int8_t> atomiki[cache_line_size*cpus.size()];
   for(auto& a:atomiki)
     a.store(0);
 
@@ -125,6 +130,9 @@ uint64_t multi_thread_atomic(
     std::thread([&,c]()
       {
 	set_affinity(cpus[c]);
+	std::atomic<int8_t>* atomiki_in=atomiki;
+	if (!share)
+	  atomiki_in = &atomiki[cache_line_size*c];
 	uint64_t time = 0;
 	for (size_t k=0; k<hard_repeats; k++)
 	  {
@@ -136,7 +144,7 @@ uint64_t multi_thread_atomic(
 	    size_t j=c;
 	    for (size_t i=0; i<64*256*100; i+=cpu_count)
 	      {
-		atomiki[j]++;
+		atomiki_in[j]++;
 		j+=cpu_count;
 		if (j>=range) j-=range;
 	      }
@@ -228,12 +236,16 @@ int main(int argc, char** argv)
 	  printf("%2d ",i);
 	}
       printf("\n");
+      time = multi_thread_regular(vec, false);
+      printf("regular(split)  (us)=%9lu\n",time);
       time = multi_thread_regular(vec);
-      printf("regular (us)=%9lu\n",time);
+      printf("regular(shared) (us)=%9lu\n",time);
+      time = multi_thread_atomic(vec, false);
+      printf("atomic(split)   (us)=%9lu\n",time);
       time = multi_thread_atomic(vec);
-      printf("atomic (us) =%9lu\n",time);
-      time = multi_thread_atomic_x(vec);
-      printf("ATOMIC (us) =%9lu\n",time);
+      printf("atomic(shared)  (us)=%9lu\n",time);
+      //time = multi_thread_atomic_x(vec);
+      //printf("ATOMIC (us) =%9lu\n",time);
     }
   }
 
@@ -243,7 +255,15 @@ int main(int argc, char** argv)
       {
 	uint64_t time;
 	std::vector<int> vec{i,j};
-	time = multi_thread_atomic_x(vec,nullptr,100);
-	printf("%d %d | time = %lu\n", i, j, time);
+	time = multi_thread_regular(vec, false);
+	printf("%d %d | reg_separate_time = %9lu\n", i, j, time);
+	time = multi_thread_atomic(vec, false);
+	printf("%d %d | ato_separate_time = %9lu\n", i, j, time);	
+	time = multi_thread_regular(vec);
+	printf("%d %d | reg_shared_time =   %9lu\n", i, j, time);
+	time = multi_thread_atomic(vec);
+	printf("%d %d | reg_shared_time =   %9lu\n", i, j, time);
+	//time = multi_thread_atomic_x(vec,nullptr,100);
+	//printf("%d %d | time = %lu\n", i, j, time);
       }
 }
