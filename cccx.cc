8e7e91f15dd91eb1d96aefa1642fd00fd31e141a
Adam Kupczyk <akupczyk@mirantis.com>
2017-04-24 14:18:41 +0200

    changed test scope

--- cccx.cc
+++ cccx.cc
@@ -57,125 +57,16 @@ int inc(char* p){
   return write_count*200+write_spread*2+(int)shared;
 };
 
+template <size_t write_count, size_t write_spread, bool shared>
+int atominc(std::atomic<int8_t>* p) __attribute__((noinline));
+template <size_t write_count, size_t write_spread, bool shared>
+int atominc(std::atomic<int8_t>* p){
+  (*p)++;
+  return write_count*200+write_spread*2+(int)shared;
+};
 
 
 
-uint64_t multi_thread_regular(
-    std::vector<int>& cpus,
-    bool share=true)
-{
-  std::atomic<uint32_t> first_done[cpus.size()];
-  for(auto& a:first_done)
-    a.store(0);
-  std::atomic<uint64_t> time_a(0);
-
-  alignas(cache_line_size) char buffer[cache_line_size*sizeof(cpus)]={0,};
-  memset(buffer, 0, cache_line_size);
-
-  std::vector<std::thread> threads;
-  int cpu_count = cpus.size();
-  size_t range = cache_line_size/cpu_count*cpu_count;
-  for (int c=0; c<cpu_count; c++)
-  {
-    threads.push_back(
-        std::thread([&,c]()
-            {
-      char* large = (char*)malloc(2*1024*1024);
-      char* buffer_in=buffer;
-      if (!share)
-        buffer_in = &buffer[cache_line_size*c];
-      set_affinity(cpus[c]);
-      uint64_t time = 0;
-      for (size_t k=0; k<hard_repeats; k++)
-      {
-        first_done[(c+1)%cpu_count]++;
-        while(first_done[c].load() == 0);
-
-        time -= now_usec();
-        size_t j=c;
-        size_t r=0;
-        for (size_t i=0; i<64*256*100; i+=cpu_count)
-        {
-          for( int ll=0;ll<0;ll++)
-          {
-            large[r] = 1;
-            r=r+65;
-            if(r>2*1024*1024) r-=2*1024*1024;
-          }
-          inc(&buffer_in[j]);
-          j+=cpu_count;
-          if (j>=range) j-=range;
-        }
-        time+= now_usec();
-        first_done[c]--;
-
-      }
-      time_a += time;
-            }
-
-        )
-    );
-  };
-  for(auto& t:threads)
-    t.join();
-  return time_a.load();
-}
-
-
-
-
-uint64_t multi_thread_atomic(
-			     std::vector<int>& cpus,
-			     bool share=true)
-{
-  std::atomic<uint32_t> first_done[cpus.size()];
-  for(auto& a:first_done)
-    a.store(0);
-
-  std::atomic<uint64_t> time_a(0);
-
-  alignas(cache_line_size) std::atomic<int8_t> atomiki[cache_line_size*cpus.size()];
-  for(auto& a:atomiki)
-    a.store(0);
-
-  std::vector<std::thread> threads;
-  int cpu_count = cpus.size();
-  size_t range = cache_line_size/cpu_count*cpu_count;
-  for (int c=0; c<cpu_count; c++)
-    {
-  threads.push_back(
-    std::thread([&,c]()
-      {
-	set_affinity(cpus[c]);
-	std::atomic<int8_t>* atomiki_in=atomiki;
-	if (!share)
-	  atomiki_in = &atomiki[cache_line_size*c];
-	uint64_t time = 0;
-	for (size_t k=0; k<hard_repeats; k++)
-	  {
-	    first_done[(c+1)%cpu_count]++;
-	    while(first_done[c].load() == 0);
-	    first_done[c]--;
-	    
-	    time -= now_usec();	
-	    size_t j=c;
-	    for (size_t i=0; i<64*256*100; i+=cpu_count)
-	      {
-		atomiki_in[j]++;
-		j+=cpu_count;
-		if (j>=range) j-=range;
-	      }
-	    time+= now_usec();
-	  }
-	time_a += time;
-      }
-    )
-  );
-    };
-  for(auto& t:threads)
-    t.join();
-  return time_a.load();
-}
 
 
 template <size_t write_count, size_t write_spread, bool share, bool do_atomic>
@@ -228,7 +119,7 @@ uint64_t generic_test(
             if(r>test_area_size) r-=test_area_size;
           }
           if (do_atomic)
-            atomiki_in[j]++;
+            atominc<write_count, write_spread, share>(&atomiki_in[j]);
           else
             inc<write_count, write_spread, share>(&buffer_in[j]);
           j+=cpu_count;
@@ -266,22 +157,6 @@ struct test_t {
 #define TEST_T(a,b,c,d) {a,b,c,d,generic_test<a,b,c,d>}
 
 std::vector<test_t> regular_separate{
-  TEST_T(0, 64, false, false),
-  TEST_T(5, 64, false, false),
-  TEST_T(10, 64, false, false),
-  TEST_T(15, 64, false, false),
-  TEST_T(20, 64, false, false),
-  TEST_T(25, 64, false, false),
-  TEST_T(30, 64, false, false),
-
-  TEST_T(0, 64, true, false),
-  TEST_T(5, 64, true, false),
-  TEST_T(10, 64, true, false),
-  TEST_T(15, 64, true, false),
-  TEST_T(20, 64, true, false),
-  TEST_T(25, 64, true, false),
-  TEST_T(30, 64, true, false),
-
   TEST_T(0, 4, false, false),
   TEST_T(5, 4, false, false),
   TEST_T(10, 4, false, false),
@@ -314,7 +189,69 @@ std::vector<test_t> regular_separate{
   TEST_T(25, 14, true, false),
   TEST_T(30, 14, true, false),
 
+  TEST_T(0, 64, false, false),
+  TEST_T(5, 64, false, false),
+  TEST_T(10, 64, false, false),
+  TEST_T(15, 64, false, false),
+  TEST_T(20, 64, false, false),
+  TEST_T(25, 64, false, false),
+  TEST_T(30, 64, false, false),
 
+  TEST_T(0, 64, true, false),
+  TEST_T(5, 64, true, false),
+  TEST_T(10, 64, true, false),
+  TEST_T(15, 64, true, false),
+  TEST_T(20, 64, true, false),
+  TEST_T(25, 64, true, false),
+  TEST_T(30, 64, true, false),
+
+  TEST_T(0, 4, false, true),
+  TEST_T(5, 4, false, true),
+  TEST_T(10, 4, false, true),
+  TEST_T(15, 4, false, true),
+  TEST_T(20, 4, false, true),
+  TEST_T(25, 4, false, true),
+  TEST_T(30, 4, false, true),
+
+  TEST_T(0, 4, true, true),
+  TEST_T(5, 4, true, true),
+  TEST_T(10, 4, true, true),
+  TEST_T(15, 4, true, true),
+  TEST_T(20, 4, true, true),
+  TEST_T(25, 4, true, true),
+  TEST_T(30, 4, true, true),
+
+  TEST_T(0, 14, false, true),
+  TEST_T(5, 14, false, true),
+  TEST_T(10, 14, false, true),
+  TEST_T(15, 14, false, true),
+  TEST_T(20, 14, false, true),
+  TEST_T(25, 14, false, true),
+  TEST_T(30, 14, false, true),
+
+  TEST_T(0, 14, true, true),
+  TEST_T(5, 14, true, true),
+  TEST_T(10, 14, true, true),
+  TEST_T(15, 14, true, true),
+  TEST_T(20, 14, true, true),
+  TEST_T(25, 14, true, true),
+  TEST_T(30, 14, true, true),
+
+  TEST_T(0, 64, false, true),
+  TEST_T(5, 64, false, true),
+  TEST_T(10, 64, false, true),
+  TEST_T(15, 64, false, true),
+  TEST_T(20, 64, false, true),
+  TEST_T(25, 64, false, true),
+  TEST_T(30, 64, false, true),
+
+  TEST_T(0, 64, true, true),
+  TEST_T(5, 64, true, true),
+  TEST_T(10, 64, true, true),
+  TEST_T(15, 64, true, true),
+  TEST_T(20, 64, true, true),
+  TEST_T(25, 64, true, true),
+  TEST_T(30, 64, true, true),
 };
 
 int main(int argc, char** argv)
@@ -333,35 +270,10 @@ int main(int argc, char** argv)
 	  printf("%2d ",i);
 	}
       printf("\n");
-      time = generic_test<10, 65, false, false>(
-          test_area_size,
-          test_area,
-          vec);
-      printf("regular(split)  (us)=%9lu\n",time);
-//      time = multi_thread_regular(vec);
-      time = generic_test<10, 65, true, false>(
-          test_area_size,
-          test_area,
-          vec);
-      printf("regular(shared) (us)=%9lu\n",time);
-      time = generic_test<10, 65, false, true>(
-          test_area_size,
-          test_area,
-          vec);
-
-//      time = multi_thread_atomic(vec, false);
-      printf("atomic(split)   (us)=%9lu\n",time);
-      time = generic_test<10, 65, true, true>(
-                test_area_size,
-                test_area,
-                vec);
-//      time = multi_thread_atomic(vec);
-      printf("atomic(shared)  (us)=%9lu\n",time);
-
       for (auto &e : regular_separate)
       {
         time = e.test(test_area_size, test_area, vec);
-        printf("%2lu %2lu %d %9lu\n", e.write_count, e.write_spread, (int)e.share, time);
+        printf("%2lu %2lu %s %s %9lu\n", e.write_count, e.write_spread, e.share?"shared  ":"separate", e.do_atomic?"atomic":"normal", time);
       }
     }
 }
